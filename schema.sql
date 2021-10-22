468a7ae4baaf80fbb83aac79ef441591ef8d4746
cupofjoee <jteddysanjaya@gmail.com>
2021-10-22 14:29:42 +0800

    schema

--- schema.sql
+++ schema.sql
@@ -48,34 +48,53 @@ CASCADE;
 
 CREATE TABLE HealthDeclarations (
     -- Teddy 
+    eid     INT,
+    temp    float   NOT NULL,
+    `date`  DATE,
+    PRIMARY KEY (`date`, eid)
+    FOREIGN KEY eid REFERENCES Employees(eid)
 );
 
 
 CREATE TABLE Employees (
     -- Teddy
+    eid             INT             PRIMARY KEY,
+    ename           VARCHAR(50),
+    email           VARCHAR(50),
+    resigned_date   DATE,
+    did             INT,
+    FOREIGN KEY did REFERENCES Departments(did)
 );
 
+CREATE TABLE Contacts (
+    contact_number  VARCHAR(50),
+    eid             INT,
+    PRIMARY KEY (eid, contact_number)
+    FOREIGN KEY eid REFERENCES Employees(eid)
+)
 
 CREATE TABLE Juniors (
     -- Teddy
+    eid     INT     PRIMARY KEY,
+    FOREIGN KEY eid REFERENCES Employees(eid)
 );
 
 
 CREATE TABLE Bookers (
     -- Simon
-    eid INT REFERENCES Employees(eid)
+    eid     INT     REFERENCES Employees(eid)
 );
 
 
 CREATE TABLE Seniors (
     -- Simon
-    eid INT REFERENCES Booker(eid)
+    eid     INT     REFERENCES Booker(eid)
 );
 
 
 CREATE TABLE Managers (
     -- Simon
-    eid INT REFERENCES Booker(eid)
+    eid     INT     REFERENCES Booker(eid)
 );
 
 
@@ -100,21 +119,23 @@ CREATE TABLE MeetingRooms (
 
 CREATE TABLE Joins (
     -- Teddy 
-);
-
-
-CREATE TABLE WorksIn (
-    -- Teddy
-    -- REMARK: Merge into Employees?
+    eid     INT
+    `time`  TIME
+    `date`  DATE
+    room    INT
+    `floor` INT
+    PRIMARY KEY (eid, `time`, `date`, room, `floor`)
+    FOREIGN KEY eid REFERENCES Employees(eid)
+    FOREIGN KEY (room, `floor`) REFERENCES MeetingRooms(room, `floor`)
 );
 
 
 CREATE TABLE Updates (
     -- Simon
-    eid INT REFERENCES Managers(eid),
-    `date` DATE,
-    floor INT,
-    room INT,
+    eid     INT     REFERENCES Managers(eid),
+    `date`  DATE,
+    floor   INT,
+    room    INT,
     FOREIGN KEY (floor, room) REFERENCES MeetingRooms(floor, room)
 );
 
